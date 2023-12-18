9ef3f7a5af15de33dc5137dc37c9ebbc4dcdbd22
Big 250 <rg156649@gmail.com>
2023-12-18 21:21:02 +0800

    Update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,5 @@
 <x-app-layout>
-  {{-- <x-slot name="header">
-    <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-      {{ __('Dashboard') }}
-  </h2>
-  </x-slot> --}}
-
-
-  <div class=" sm:p-2 lg:p-4">
+  <div class="sm:p-2 lg:p-4">
     <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
       <div class="p-6 text-gray-900 dark:text-gray-100">
         {{ __("You're logged in!") }}
