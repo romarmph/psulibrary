548f4285573cf6c7d9f2fbc51417d33702ae0e78
Big 250 <rg156649@gmail.com>
2023-12-21 23:20:00 +0800

    Refactor RecentBorrowsTable component to include search and category filters

--- app/Livewire/Components/RecentBorrowsTable.php
+++ app/Livewire/Components/RecentBorrowsTable.php
@@ -2,7 +2,7 @@
 
 namespace App\Livewire\Components;
 
-use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -12,31 +12,49 @@ class RecentBorrowsTable extends Component
   use WithPagination;
 
   public $perPage = 10;
+  public $search = '';
+  public $category = 0;
 
   public function render()
   {
+    $bookCategories = Category::all();
     $borrows = DB::table('borrowed_books')
       ->join('borrow_details', 'borrow_details.id', '=', 'borrowed_books.borrow_detail_id')
       ->join('books', 'books.id', '=', 'borrowed_books.book_id')
       ->join('users', 'users.id', '=', 'borrow_details.issued_by')
+      ->join('categories', 'categories.id', '=', 'books.category_id')
       ->select(
+        'users.id_number',
         'users.first_name',
         'users.last_name',
         'books.title',
         'books.isbn',
+        'categories.name as category',
         'borrowed_books.quantity',
         'borrow_details.borrowed_from_date',
         'borrow_details.borrowed_to_date',
       )
-      ->where('borrow_details.borrowed_from_date', '>=', now()->subDays(3))
-      ->orderBy('borrow_details.borrowed_from_date', 'desc')
-      ->paginate(10);
+      ->where('borrow_details.borrowed_from_date', '>=', now()->subDays(7))
+      ->where('books.title', 'like', '%' . $this->search . '%')
+      ->orWhere('books.isbn', 'like', '%' . $this->search . '%')
+      ->orWhere('users.first_name', 'like', '%' . $this->search . '%')
+      ->orWhere('users.last_name', 'like', '%' . $this->search . '%')
+      ->orWhere('users.id_number', 'like', '%' . $this->search . '%')
+      ->orderBy('borrow_details.borrowed_from_date', 'desc');
 
-    dd($borrows);
+
+
+    if ($this->category != 0) {
+      $borrows = $borrows->where('books.category_id', '=', $this->category);
+    }
+
+    $borrows = $borrows->paginate($this->perPage);
+
     return view(
       'livewire.components.recent-borrows-table',
       [
         'borrows' => $borrows,
+        'categories' => $bookCategories,
       ],
     );
   }
--- resources/views/livewire/components/recent-borrows-table.blade.php
+++ resources/views/livewire/components/recent-borrows-table.blade.php
@@ -1,7 +1,6 @@
 <div>
-  <section class="mt-10">
-    <div class="max-w-screen-xl px-4 mx-auto lg:px-12">
-      <!-- Start coding here -->
+  <section class="mt-10" wire:poll.1000ms>
+    <div class="mx-auto ">
       <div class="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
         <div class="flex items-center justify-between p-4 d">
           <div class="flex">
@@ -11,16 +10,17 @@
                   <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
                 </svg>
               </div>
-              <input type="text" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 " placeholder="Search" required="">
+              <input wire:model.live='search' type="text" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 " placeholder="Search" required="">
             </div>
           </div>
           <div class="flex space-x-3">
             <div class="flex items-center space-x-3">
-              <label class="w-40 text-sm font-medium text-gray-900">User Type :</label>
-              <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
-                <option value="">All</option>
-                <option value="0">User</option>
-                <option value="1">Admin</option>
+              <label class="w-40 text-sm font-medium text-gray-900">Category</label>
+              <select wire:model.live='category' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                <option value="0">All</option>
+                @foreach ($categories as $category)
+                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
               </select>
             </div>
           </div>
@@ -29,39 +29,53 @@
           <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50">
               <tr>
-                <th scope="col" class="px-4 py-3">name</th>
-                <th scope="col" class="px-4 py-3">email</th>
-                <th scope="col" class="px-4 py-3">Role</th>
-                <th scope="col" class="px-4 py-3">Joined</th>
-                <th scope="col" class="px-4 py-3">Last update</th>
+                <th scope="col" class="px-4 py-3">
+                  <button class="w-full">ID Number</button>
+                </th>
+                <th scope="col" class="px-4 py-3">Name</th>
+                <th scope="col" class="px-4 py-3">Book Title</th>
+                <th scope="col" class="px-4 py-3">Book ISBN</th>
+                <th scope="col" class="px-4 py-3">Category</th>
+                <th scope="col" class="px-4 py-3">Quantity</th>
+                <th scope="col" class="px-4 py-3">Date Issued</th>
+                <th scope="col" class="px-4 py-3">Date Due</th>
                 <th scope="col" class="px-4 py-3">
                   <span class="sr-only">Actions</span>
                 </th>
               </tr>
             </thead>
             <tbody>
+              @foreach ($borrows as $borrow)
               <tr class="border-b dark:border-gray-700">
-                <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  name</th>
-                <td class="px-4 py-3">email</td>
-                <td class="px-4 py-3 text-green-500">
-                  admin</td>
-                <td class="px-4 py-3">created_at</td>
-                <td class="px-4 py-3">updated_at</td>
-                <td class="flex items-center justify-end px-4 py-3">
-                  <button class="px-3 py-1 text-white bg-red-500 rounded">X</button>
+
+                <td class="px-4 py-3 text-lg">{{ $borrow->id_number }}</td>
+                <td class="px-4 py-3 text-lg">{{ $borrow->first_name.' '.$borrow->last_name }}</td>
+                <td class="px-4 py-3 text-lg">{{ $borrow->title }}</td>
+                <td class="px-4 py-3 text-lg">{{ $borrow->isbn }}</td>
+                <td class="px-4 py-3 text-lg">{{ $borrow->category }}</td>
+                <td class="px-4 py-3 text-lg">{{ $borrow->quantity }}</td>
+                <td class="px-4 py-3 text-lg">
+                  {{ date('F j, Y, g:i a', strtotime($borrow->borrowed_from_date)) }}
+                </td>
+                <td class="px-4 py-3 text-lg">
+                  {{ date('F j, Y, g:i a', strtotime($borrow->borrowed_to_date)) }}
+                </td>
+                <td class="px-4 py-3">
+                  <a href="#" class="px-3 py-2 text-white bg-green-500 rounded-lg">
+                    View
+                  </a>
                 </td>
               </tr>
-
+              @endforeach
             </tbody>
           </table>
         </div>
 
         <div class="px-3 py-4">
-          <div class="flex ">
+          <div class="flex items-center justify-between">
             <div class="flex items-center mb-3 space-x-4">
               <label class="w-32 text-sm font-medium text-gray-900">Per Page</label>
-              <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+              <select wire:model.live='perPage' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
                 <option value="5">5</option>
                 <option value="10">10</option>
                 <option value="20">20</option>
@@ -69,10 +83,10 @@
                 <option value="100">100</option>
               </select>
             </div>
+            {{ $borrows->links() }}
           </div>
         </div>
       </div>
     </div>
   </section>
-
 </div>
