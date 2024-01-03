597ebce691dd4332669e690befe4049baf5aa833
Romar Macaraeg <roamrcm@gmail.com>
2024-01-04 01:34:22 +0800

    Update books-page.blade.php layout

--- resources/views/livewire/admin/books-page.blade.php
+++ resources/views/livewire/admin/books-page.blade.php
@@ -1,16 +1,11 @@
 <div class="p-4">
 
   <x-slot name="header">
-    <h1 class="text-2xl font-medium">Dashboard</h1>
+    <h1 class="text-2xl font-medium">Books</h1>
   </x-slot>
-  <div class="grid grid-cols-1 gap-4 mb-4 lg:grid-cols-2 xl:grid-cols-4">
-    <livewire:components.dashboard-card :icon="'fluentui-book-20'" :iconBgColor="'bg-blue-500'" :iconColor="'text-white'" :title="'Total Books'" :value="'100'" />
-    <livewire:components.dashboard-card :icon="'fluentui-book-coins-20'" :iconBgColor="'bg-green-500'" :iconColor="'text-white'" :title="'Current Books'" :route="''" :value="'75'" />
-    <livewire:components.dashboard-card :icon="'fluentui-book-clock-20'" :iconBgColor="'bg-red-500'" :iconColor="'text-white'" :title="'Total Borrowed Books'" :value="'25'" />
-    <livewire:components.dashboard-card :icon="'fluentui-person-clock-20'" :iconBgColor="'bg-purple-500'" :iconColor="'text-white'" :title="'Total Borrowers'" :route="''" :value="'13'" />
-  </div>
-  <livewire:flash.success>
-    <div class="mt-4">
-      <livewire:components.books-table />
-    </div>
+  <livewire:components.books-quick-info>
+    <livewire:flash.success>
+      <div class="mt-4">
+        <livewire:components.books-table />
+      </div>
 </div>
