245bb8ccdaf722759e1bd317538c1350b1d156b8
Romar Macaraeg <roamrcm@gmail.com>
2024-01-06 05:07:43 +0800

    Update pagination and add limit selection on borrower books page

--- resources/views/livewire/borrower/books/borrower-books-page.blade.php
+++ resources/views/livewire/borrower/books/borrower-books-page.blade.php
@@ -76,8 +76,19 @@
   </div>
 
 
-  <div class="px-4 py-4 my-8">
-    {{ $books->links() }}
+  <div class="flex justify-between px-4 py-4 my-8 ">
+    <div class="w-28">
+      <select wire:model.live="limit" name="published_at" id="published_at" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <option value="10">10</option>
+        <option value="20">20</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+
+      </select>
+    </div>
+    <div class="">
+      {{ $books->links() }}
+    </div>
   </div>
 
 
