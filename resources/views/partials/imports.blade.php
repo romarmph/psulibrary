6856f56ad449ecd3a3112675c1ccc6cee164a330
Romar Macaraeg <roamrcm@gmail.com>
2024-01-08 07:38:29 +0800

    Update CSS and JavaScript imports

--- resources/views/partials/imports.blade.php
+++ resources/views/partials/imports.blade.php
@@ -16,10 +16,10 @@
 {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
 <!-- Scripts -->
 
-{{-- <link rel="stylesheet" href="{{ asset('build/assets/app-b2b172a9.css') }}">
-<link rel="stylesheet" href="{{ asset('build/assets/app-ea55dbb8.css') }}"> --}}
+<link rel="stylesheet" href="{{ asset('build/assets/app-9f0258d6.css.css') }}">
+<link rel="stylesheet" href="{{ asset('build/assets/app-b2b172a9.css.css') }}">
 
-{{-- <script src="{{ asset('build/assets/app-e68240d5.js') }}"></script> --}}
+<script src="{{ asset('build/assets/app-1c195aa5.js') }}"></script>
 
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+{{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
