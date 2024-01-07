6dc5b64a39433c3a860b1337ebfe3db323df052e
Romar Macaraeg <roamrcm@gmail.com>
2024-01-07 13:09:18 +0800

    Add books to cart in CartPage

--- app/Livewire/Borrower/CartPage.php
+++ app/Livewire/Borrower/CartPage.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire\Borrower;
 
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
 
 class CartPage extends Component
 {
@@ -10,10 +12,19 @@ class CartPage extends Component
   {
     $user = auth()->user();
 
+    $booksInCart = DB::table('carts')
+      ->join('books', 'carts.book_id', '=', 'books.id')
+      ->select('books.*', 'carts.quantity')
+      ->where('carts.user_id', '=', $user->id)
+      ->get();
+
+    dd($booksInCart);
+
     return view(
       'livewire.borrower.cart-page',
       [
         'user' => $user,
+        'books' => $booksInCart,
       ],
     )->layout('layouts.borrower', ['title' => 'PSU Library']);
   }
---
+++ app/Livewire/Components/CartItemCard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Components;
+
+use Livewire\Component;
+
+class CartItemCard extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.cart-item-card');
+    }
+}
